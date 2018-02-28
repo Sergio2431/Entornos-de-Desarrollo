438f90ae901ff2876c78468862a7a418fb6ec2f3
ALUMNADO\seruba <sergio_311096@hotmail.es>
2018-02-28 09:55:12 +0100

    subir archivo

---
+++ PHP/salvarjudios.php
@@ -0,0 +1,7 @@
+<?php
+
+$fraseCodificada="Los catalenes hacen cosas";
+$fraseTemp="";
+
+$fraseTemp=$fraseTemp. substr($fraseCodificada, $fraseTemp+1,1). substr($fraseCodificada, $fraseTemp+1,1);
+ ?>
